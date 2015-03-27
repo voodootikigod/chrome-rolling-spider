237832e9b241d683737b08adf0263eb74d5abb27
Chris Williams <chris@iterativedesigns.com>
2015-03-26 20:32:25 -0400

    Flushing changes from the recent push including enumerating the UUIDs for the services that we want to access. Initialization works, handoff is still suspect. For now.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var Drone = function(options) {
 };
 
 
+
 // create client helper function to match ar-drone
 Drone.createClient = function (options) {
   return new Drone(options);
@@ -17,7 +18,7 @@ var searchTimer;
 
 function stopDiscovery() {
   try {
-    if (searchTimer) { clearTimeout(searchTimer) }
+    if (searchTimer) { clearTimeout(searchTimer); }
     chrome.bluetooth.stopDiscovery();
   } catch (e) {
     console.error('Issue with ending discovery');
@@ -35,6 +36,7 @@ Drone.prototype.bleConnect = function (device, callback) {
       console.log('Connected to rolling spider');
       that.peripheral = device;
       that.connected = true;
+      that.address = device.address;
       if (callback)
         callback(null, that);
     }
@@ -45,7 +47,9 @@ Drone.prototype.connect = function(callback) {
 
 
   var deviceFound = (function (device) {
-    if (device.address === this.address) {
+    if (device.name === "RS_W008267") {
+      console.log('skipping '+"RS_W008267");
+    } else if (device.address === this.address) {
       this.bleConnect(device, callback);
     } else if ((typeof this.address) === 'undefined' &&
       device.name.indexOf('RS_') === 0) {
@@ -69,42 +73,102 @@ Drone.prototype.connect = function(callback) {
 };
 
 
+function discoverAllServicesAndCharacteristics(address, callback) { // callback(error, services, characteristics);
+  chrome.bluetoothLowEnergy.getServices(address, function (services) {
+    var characteristics = [];
+    var end = (function (max) {
+      var count = 0;
+      return function (chrcs) {
+        if (chrome.runtime.lastError) {
+          console.log(chrome.runtime.lastError.message);
+          callback(chrome.runtime.lastError);
+          return;
+        }
+
+        // Make sure that the same service is still selected.
+        //if (service.instanceId != self.service_.instanceId) {
+        //  return;
+        //}
+
+        if (!chrcs.length) {
+          log('Service has no characteristics: ' + service.instanceId);
+          return;
+        }
+        count += 1;
+        characteristics = characteristics.concat(chrcs || []);
+        if (count === max && callback) {
+          callback(null, services, characteristics);
+        }
+      };
+    })(services.length);
+
+    services.forEach(function(service) {
+      chrome.bluetoothLowEnergy.getCharacteristics(service.instanceId, end);
+    });
+  });
+
+  
+}
+
 
 Drone.prototype.setup = function(callback) {
   var that = this;
-  chrome.bluetoothLowEnergy.getServices(that.peripheral.address, function (services) {
+
+  discoverAllServicesAndCharacteristics(that.peripheral.address, function (error, services, characteristics) {
     that.services = services;
-    console.log('Services', services);
-    services.forEach(function(service) {
-      chrome.bluetoothLowEnergy.getCharacteristics(service.instanceId, function (characteristics) {
-        console.log(characteristics);
-        that.characteristics = characteristics;
-      });
-    });
+    that.characteristics = characteristics;
+    if (callback)
+      callback();
   });
+  
 };
 
 
 Drone.prototype.handshake = function(callback) {
-  this.getCharacteristic('fb0f').notify(true);
-  this.getCharacteristic('fb0e').notify(true);
-  this.getCharacteristic('fb1b').notify(true);
-  this.getCharacteristic('fb1c').notify(true);
-  this.getCharacteristic('fd22').notify(true);
-  this.getCharacteristic('fd23').notify(true);
-  this.getCharacteristic('fd24').notify(true);
-  this.getCharacteristic('fd52').notify(true);
-  this.getCharacteristic('fd53').notify(true);
-  this.getCharacteristic('fd54').notify(true);
+  var characteristicNotificationHandler = function (drone, characteristic) {
+    return function () {
+      if (chrome.runtime.lastError) {
+        if (chrome.runtime.lastError.message === 'In progress') {
+          //silent
+        } else {
+          console.log('err: ' + chrome.runtime.lastError.message);
+        }
+      } else {
+        console.log('characteristic update', characteristic);
+      }
+    };
+  };
+  
+  var listening = [ 'fb8f', 'fb8e', 'fb1b', 'fb1c', 'fd22', 'fd23', 'fd24', 
+                    'fd52', 'fd53', 'fd54' ];
+  //register all notifications 
+  listening.forEach(function(key) {
+    var characteristic = this.getCharacteristic(key);
+    if (!characteristic) {
+      console.log('Not found ', key);
+    } else {
+      chrome.bluetoothLowEnergy.startCharacteristicNotifications(characteristic.instanceId, characteristicNotificationHandler(this, characteristic));
+    }
+  }.bind(this));
 
 
   setTimeout(function() {
-    this.steps['fa0b'] = (this.steps['fa0b'] || 0) + 1;
-
-    this.getCharacteristic('fa0b').write(
-        new Buffer([0x04,this.steps['fa0a'],0x00,0x04,0x01,0x00,0x32,0x30,0x31,0x34,0x2D,0x31,0x30,0x2D,0x32,0x38,0x00]),
-        true,
-        function(error) {
+    var key = 'fa0b';
+    this.steps[key] = (this.steps[key] || 0) + 1;
+    var characteristic = this.getCharacteristic(key);
+    
+    
+    var msg = buffer([ 0x04, this.steps['fa0a'], 0x00, 0x04, 0x01, 0x00, 0x32, 
+                       0x30, 0x31, 0x34, 0x2D, 0x31, 0x30, 0x2D, 0x32, 0x38, 0x00]);
+    
+    console.log(characteristic);
+    chrome.bluetoothLowEnergy.writeCharacteristicValue(characteristic.instanceId,
+        msg,
+        function() {
+          if (chrome.runtime.lastError) {
+            console.log('err: ' + chrome.runtime.lastError.message);
+            
+          }
           setTimeout(function() {
             if (callback)
               callback();
@@ -129,6 +193,16 @@ Drone.prototype.getCharacteristic = function(unique_uuid_segment) {
 
 
 
+function buffer(array) {
+  var ab = new ArrayBuffer(array.length);
+  var ua = new Uint8Array(ab);
+  for (var i = 0; i < array.length; i++) {
+    ua[i] = array[i];
+  }
+  return ab;
+}
+
+
 
 
 
@@ -137,7 +211,7 @@ var powered = false;
 chrome.bluetooth.getAdapterState(function(adapter) {
   if (!adapter.powered) {
     alert('Please enable bluetooth on the device');
-  };
+  }
 });
 
 
@@ -152,7 +226,15 @@ window.onload = function() {
     if (error) {
       console.error(error);
     } else {
-      drone.setup();
+      drone.setup(function (error) {
+        if (error) {
+          console.error(error);
+        } else {
+          drone.handshake(function (error) {
+            console.log(drone);  
+          });
+        }
+      });
     }
   });
 

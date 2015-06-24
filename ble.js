96399bc245283423392a06e1964492d5597496ff
Chris Williams <chris@iterativedesigns.com>
2015-06-24 10:25:38 -0400

    Consolidate down ble shim.

--- ble.js
+++ ble.js
@@ -1,11 +1,101 @@
 /* global chrome */
 
-var BLE = function (options) {
-  this.listeners = {};
+var BLECharacteristic = function (rawCharacteristic) {
+  this.characteristic = rawCharacteristic;
+  this.uuid = rawCharacteristic.uuid;
+}
+
+BLECharacteristic.prototype.write = function (buffer, response, callback) {
+  chrome.bluetoothLowEnergy.writeCharacteristicValue(this.characteristic.id, buffer, callback);
 };
 
 
 
+
+var BLEDevice = function (rawDevice) {
+  this.advertisement = {
+    localName: rawDevice.name,
+    manufacturerData: null,
+    txPowerLevel: undefined,
+    serviceData: [],
+    serviceUuids: []
+  };
+  this.listeners = {};
+  this.address = rawDevice.address;
+  this.rssi = undefined;
+  this.services = undefined;
+  this.state = 'disconnected';
+  this.uuid = (rawDevice.uuids[0] || '').replace(/[^a-f0-9]/g, '');
+}
+
+
+BLEDevice.prototype.connect = function (callback) {
+  chrome.bluetoothLowEnergy.connect(this.address, function () {
+    if (chrome.runtime.lastError) {
+      console.error('Failed to connect ' + this.uuid + ': ' + chrome.runtime.lastError.message);
+      if (callback)
+        callback(chrome.runtime.lastError);
+    } else {
+      if (typeof callback === 'function') {
+        callback(null, this);
+      }
+    }
+  }.bind(this));
+};
+
+BLEDevice.prototype.on = function (key, handler) {
+  if (!this.listeners[key]) {
+    this.listeners[key] = [];
+  }
+  this.listeners[key].push(handler);
+  return;
+};
+
+BLEDevice.prototype.updateRssi = function (callback) {
+    callback(null);
+};
+
+
+BLEDevice.prototype.disconnect = function (callback) {
+  chrome.bluetoothLowEnergy.disconnect(this.address, callback);
+};
+
+BLEDevice.prototype.discoverAllServicesAndCharacteristics = function (callback) {
+  chrome.bluetoothLowEnergy.getServices(this.address, function (services) {
+    var characteristics = [];
+    var end = (function (max) {
+      var count = 0;
+      return function (chrcs) {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError.message);
+          callback(chrome.runtime.lastError);
+          return;
+        }
+        if (!chrcs.length) {
+          console.log('Service with no characteristics!');
+          return;
+        }
+        count += 1;
+        for (var idx = 0, l = chrcs.length; idx < l; idx++) {
+          characteristics.push(new BLECharacteristic(chrcs[idx]));
+        }
+        if (count === max && callback) {
+          callback(null, services, characteristics);
+        }
+      };
+    })(services.length);
+
+    services.forEach(function (service) {
+      chrome.bluetoothLowEnergy.getCharacteristics(service.instanceId, end);
+    });
+  });
+};
+
+
+
+var BLE = function (options) {
+  this.listeners = {};
+};
 
 // Valid keys:
 //  * warning
--- blecharacteristic.js
+++
@@ -1,9 +0,0 @@
-
-var BLECharacteristic = function (rawCharacteristic) {
-  this.characteristic = rawCharacteristic;
-  this.uuid = rawCharacteristic.uuid;
-}
-
-BLECharacteristic.prototype.write = function (buffer, response, callback) {
-  chrome.bluetoothLowEnergy.writeCharacteristicValue(this.characteristic.id, buffer, callback);
-};
